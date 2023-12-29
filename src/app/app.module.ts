a9a005332e7588beb4e07225baf40b91901f3688
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-12-29 09:45:34 +0300

    feat: lecture 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HeaderModule} from './components/header/header.module';
+
+// Components
+// Directives
+// Pipe
+
+// Module
+
+// Services
+
+/**
+ * declarations ~ const
+ * exports ~ module.exports = {...}
+ * imports ~ import {...} from '...'
+ */
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-    providers: [],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+    readonly title = 'Angular-learnjs-211223';
+    readonly imgSrc = '../../../favicon.ico';
+
+    menuClick(event: MouseEvent) {
+        // eslint-disable-next-line no-console
+        console.log('Clicked', event);
+
+        // event.stopPropagation();
+    }
+}
