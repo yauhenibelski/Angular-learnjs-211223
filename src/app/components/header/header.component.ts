b0112dd606e9431d9aa9ad0b9587abac48a05f76
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-01-11 22:37:29 +0300

    feat: lecture 3, part 1

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ApplicationConfig} from '../../shared/application-config/application-config.interface';
 
 @Component({
     selector: 'app-header',
@@ -6,10 +7,24 @@ import {Component} from '@angular/core';
     styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-    readonly title = 'Angular-learnjs-211223';
-    readonly imgSrc = '../../../favicon.ico';
+    // readonly title = 'Angular-learnjs-211223';
+    // readonly imgSrc = '../../../favicon.ico';
 
-    menuClick(event: MouseEvent) {
+    // @Input('applicationConfig') applicationConfig: ApplicationConfig | null = null;
+    @Input() applicationConfig: ApplicationConfig | null = null;
+    // @Input('config') applicationConfig: ApplicationConfig | null = null;
+
+    // @Output() readonly menuClick = new Subject<void>();
+    // @Output() readonly menuClick = interval(100);
+    // @Output('menu') readonly menuClick = new EventEmitter<void>();
+    @Output() readonly menuClick = new EventEmitter<void>(true);
+    // @Output() readonly menuClick = new EventEmitter<MouseEvent>();
+
+    onMenuClick(event: MouseEvent) {
+        // this.menuClick.next();
+        // this.menuClick.emit(event); // emit === super.next
+        this.menuClick.emit();
+
         // eslint-disable-next-line no-console
         console.log('Clicked', event);
     }
