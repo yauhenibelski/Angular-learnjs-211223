e223639ebd83f7ced4797088a77764b9d441651b
Letto228 <learnjs@MacBook-Pro-Egor.local>
2024-01-25 09:59:17 +0300

    feat: add pagination directive

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 import {ProductsListComponent} from './products-list.component';
 import {CardModule} from './card/card.module';
 import {DumpNgIfModule} from '../../shared/dump-ng-if/dump-ng-if.module';
 import {ScrollWithLoadingModule} from '../../shared/scroll-with-loading/scroll-with-loading.module';
+import {PaginationModule} from '../../shared/pagination/pagination.module';
 
 @NgModule({
     declarations: [ProductsListComponent],
@@ -14,6 +17,9 @@ import {ScrollWithLoadingModule} from '../../shared/scroll-with-loading/scroll-w
         DumpNgIfModule,
         MatProgressSpinnerModule,
         ScrollWithLoadingModule,
+        MatButtonModule,
+        MatIconModule,
+        PaginationModule,
     ],
     exports: [ProductsListComponent],
 })
